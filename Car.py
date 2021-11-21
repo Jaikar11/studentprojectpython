0079b4e45a0dd029ac51640bbaa784bfd730df9b
Jaikar11 Linux <Jaikar11-Linux@yahoo.com>
2021-11-21 10:42:28 +0000

    0.3 linux updated merged version

--- Car.py
+++ Car.py
@@ -4,12 +4,12 @@ from Vehicle import Vehicle
 
 class Car(Vehicle):
     def carDetails(self, veh_type=None, car_make=None, car_colour=None, car_model=None):
-        super.__init__(veh_type,car_make,car_colour, car_model)  # super calls the constructor of the parent class
+        super.__init__(veh_type, car_make, car_colour, car_model)  # super calls the constructor of the parent class
 
 
-car1 = Car("Car","VW","Grey","Golf");
+print("Car class is child class of Vehicle class(Parent) ")
+car1 = Car("Car", "Volvo", "Grey", "Golf");
 car1.printDetails()
 
-
-
-
+car4 = Car("Car", "Vauxhall", "White", "Astra");
+car1.printDetails()
--- Student.py
+++ Student.py
@@ -1,26 +1,25 @@
 # Setter and Getter Class
 
 class Student:
-   # def __init__(self,name="None",rollNumber=0):
+    # def __init__(self,name="None",rollNumber=0):
     #    self._name = name
     #    self._rollnumber = 0
 
-    def setName(self,name):
+    def setName(self, name):
         self.__name = name
 
     def getName(self):
         return self.__name
 
-    def setRollNumber(self,rollnumber):
+    def setRollNumber(self, rollnumber):
         self.__rollNumber = rollnumber;
 
     def getRollNumber(self):
         return self.__rollNumber
 
+
 stu1 = Student()
 stu1.setName("Aaron")
 stu1.setRollNumber(1)
 
-print("Student Name -",stu1.getName(),"\n\tStudent Roll Number",stu1.getRollNumber())
-
-
+print("Student Name -", stu1.getName(), "\n\tStudent Roll Number", stu1.getRollNumber())
